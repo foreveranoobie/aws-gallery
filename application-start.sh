c67fce485ab25c02db02964d8939f1e8d622eb73
Oleksandr Storozhuk <ostorozhuk@ibagroup.eu>
2024-10-11 15:49:17 +0200

    CloudWatch logging added

--- application-start.sh
+++ application-start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo java -jar /home/ec2-user/apps/gallery/gallery-0.0.1-SNAPSHOT.jar &
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/tmp/amazon-cloudwatch-agent.json
+sudo nohup java -jar /home/ec2-user/apps/gallery/gallery-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+exit 0
---
+++ health_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ 
+while 'true'
+do
+    if [ "$(curl -s http://localhost:8080/actuator/health)" = '{"status":"UP"}' ]
+    then
+        echo "Application is running!"
+        exit 0
+    else
+        echo "Checking application status..."
+        sleep 3s
+    fi
+done
