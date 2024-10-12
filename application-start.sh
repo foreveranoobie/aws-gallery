c03c1c95393f4b4c9da62d01d8ad12226d242d90
Oleksandr Storozhuk <ostorozhuk@ibagroup.eu>
2024-10-12 19:37:51 +0200

    CloudWatch config init

--- application-start.sh
+++ application-start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sudo nohup java -jar /home/ec2-user/apps/gallery/gallery-0.0.1-SNAPSHOT.jar > /home/ec2-user/apps/gallery/out1 2> /home/ec2-user/apps/gallery/out2 < /dev/null &
+if [ "$APP_PROFILE" == "qa" ]
+then
+    nohup java -jar -Dspring-boot.run.profiles=qa /home/ec2-user/apps/gallery/gallery-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+else
+    nohup java -jar -Dspring-boot.run.profiles=dev /home/ec2-user/apps/gallery/gallery-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+fi
 exit 0
--- src/main/java/com/alexstk/gallery/controller/MainController.java
+++ src/main/java/com/alexstk/gallery/controller/MainController.java
@@ -1,12 +1,17 @@
 package com.alexstk.gallery.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    @Value("${spring.profiles.active}")
+    private String profileName;
+
     @GetMapping("/health")
     public String getHealth(){
-        return "Hello from EC2";
+        return String.format("Hello from EC2 on %s's profile", profileName);
     }
 }
